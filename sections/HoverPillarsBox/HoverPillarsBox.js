b744bb345cb4e5d590376bc52ffc6b9119a42151
Letícia Cellurale <leticia.cellurale@gmail.com>
2021-12-02 20:42:57 -0300

    💄 create hover pillars box section

--- sections/HoverPillarsBox/HoverPillarsBox.js
+++ sections/HoverPillarsBox/HoverPillarsBox.js
@@ -7,30 +7,38 @@ import styles from "./HoverPillarsBox.module.scss";
 const HoverPillarsBox = ({ data }) => {
   return (
     <section className={styles.hoverPillarsBox}>
-      <div className={styles.gridWrapper}>
-        <div className={styles.contentWrapper}>
-          <SectionTitle
-            color={data.Title.BackgroundColor}
-            className={styles.title}
-          >
-            {data.Title.SmallTitle}
-            <span>{data.Title.Title}</span>
-          </SectionTitle>
-          <RichText className={styles.description}>{data.Description}</RichText>
-        </div>
-        <div className={styles.pillars}>
-          {data.Pillars.map((pillar) => (
-            <div key={pillar.id} className={styles.pillar}>
-              <NextImage
-                className={styles.pillarImage}
-                media={pillar.BackgroundImage}
-                layout="fill"
-                objectFit="cover"
-              />
-              <h3 className={styles.pillarTitle}>{pillar.Title}</h3>
-              <p className={styles.pillarDescription}>{pillar.Description}</p>
-            </div>
-          ))}
+      <div className="container">
+        <div className={styles.gridWrapper}>
+          <div className={styles.contentWrapper}>
+            <SectionTitle
+              color={data.Title.BackgroundColor}
+              className={styles.title}
+            >
+              {data.Title.SmallTitle}
+              <span>{data.Title.Title}</span>
+            </SectionTitle>
+            <RichText className={styles.description}>
+              {data.Description}
+            </RichText>
+          </div>
+          <div className={styles.pillars}>
+            {data.Pillars.map((pillar) => (
+              <div key={pillar.id} className={styles.pillar}>
+                <NextImage
+                  className={styles.pillarImage}
+                  media={pillar.BackgroundImage}
+                  layout="fill"
+                  objectFit="cover"
+                />
+                <div className={styles.pillarContent}>
+                  <h3 className={styles.pillarTitle}>{pillar.Title}</h3>
+                  <p className={styles.pillarDescription}>
+                    {pillar.Description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
