9222430d2eb84f083d06e8ef74f54a90c40f22ce
Letícia Cellurale <leticia.cellurale@gmail.com>
2022-01-02 15:46:32 -0300

    🍻 add api url

--- libs/utils.js
+++ libs/utils.js
@@ -1,5 +1,5 @@
 export function getStrapiURL(path) {
-    return `${process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'
+    return `${'http://54.233.192.244/server/' || 'http://localhost:1337'
         }${path}`
 }
 
